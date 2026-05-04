3c6d88caea940176680aceb8890145d8d98e7ae2
Daniel Mewes <daniel.mewes@imbue.com>
2026-05-04 10:35:30 -0700

    Make artifacts clickable if they appear inside of an inline code block

--- src/frontend/src/components/ArtifactViewerModal.tsx
+++ src/frontend/src/components/ArtifactViewerModal.tsx
@@ -80,8 +80,13 @@ export function ArtifactViewerModal({ taskId, artifactId, onClose }: ArtifactVie
     return parts.map((part, i) => {
       if (i % 2 === 0) {
         return part.replace(/\b([ELTRXP]_\d{8}_\d{6}_[a-f0-9]{6})\b/g, `[$1](#/task/${taskId}/artifact/$1?from=artifact)`);
+      } else {
+        const match = part.match(/^`([ELTRXP]_\d{8}_\d{6}_[a-f0-9]{6})`$/);
+        if (match) {
+          return `[${part}](#/task/${taskId}/artifact/${match[1]}?from=artifact)`;
+        }
+        return part;
       }
-      return part;
     }).join('');
   };
 
