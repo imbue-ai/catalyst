d59f2d12f29070041de17ba9a3a43003d798196a
Daniel Mewes <daniel.mewes@imbue.com>
2026-05-07 09:21:04 -0700

    Bind only to localhost

--- src/server.py
+++ src/server.py
@@ -569,4 +569,4 @@ def export_artifact(task_id: str, artifact_id: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8139)
+    uvicorn.run(app, host="localhost", port=8139)
