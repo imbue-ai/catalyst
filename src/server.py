2ed9202792a3efcb6e64400d420696708e92562b
Daniel Mewes <daniel.mewes@imbue.com>
2026-05-07 09:15:38 -0700

    Change default backend port

--- src/server.py
+++ src/server.py
@@ -569,4 +569,4 @@ def export_artifact(task_id: str, artifact_id: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8939)
