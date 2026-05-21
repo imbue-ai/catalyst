367fc73ae2ffc5d7aa69cbfd9be07d4071c5b257
Daniel Mewes <daniel.mewes@imbue.com>
2026-05-21 16:34:32 -0700

    Slightly lower force expansion prob

--- src/orchestrator/workflows/common.py
+++ src/orchestrator/workflows/common.py
@@ -24,7 +24,7 @@ DEFAULT_NUM_EXTRA_SCORES = 5
 
 # Make the theory development process more efficient and adds diversity by occasionally forcing the application of expansion reviews,
 # even when there are still falsifications to be applied.
-FORCE_EXPANSION_PROB = 0.3
+FORCE_EXPANSION_PROB = 0.25
 
 
 def run_summarize_title(task: Task, run_step_fn: Callable, content_desc: str) -> None:
